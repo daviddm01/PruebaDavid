ba1e5325713f14123f8089ac24ffea4b5bf50a7d
josemi <jmarenasgallardo@iessansebastian.com>
2021-03-17 15:53:02 +0100

    Commit a eliminar

---
+++ Prueba_git/src/main/java/com/mycompany/prueba_git/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba_git;
+import java.util.Scanner;
+/**
+ *
+ * @author Josemi
+ */
+public class Prueba {
+    System.out.println("Hola buenas");
+}
